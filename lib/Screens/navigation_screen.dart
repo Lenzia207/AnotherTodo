1aa4ece283d37b6b29971ab71048540fcfdec7a9
Lena Zyadeh <lena.zyadeh@marqably.com>
2023-03-05 19:55:03 +0100

    refactoring file structure

--- lib/Screens/navigation_screen.dart
+++ lib/Screens/navigation_screen.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'package:another_todo/widgets/open_tasks_screen.dart';
+import 'package:another_todo/Screens/open_tasks_screen.dart';
 
 // TODO Add the Completed-Screen and Private-Screen
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'package:another_todo/navigation_screen.dart';
+import 'package:another_todo/Screens/navigation_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
