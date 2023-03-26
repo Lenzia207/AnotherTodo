968f2e7da4e8e050293146c631b74164440f8928
Lenzia207 <lenzia207@gmail.com>
2023-03-26 22:20:59 +0200

    add Snackbar Undo before final delete

--- lib/Screens/private_tasks_screen.dart
+++ lib/Screens/private_tasks_screen.dart
@@ -7,7 +7,6 @@ import 'package:another_todo/widgets/task_items/slide_action_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_use/flutter_use.dart';
 
 /// This is the general representation of the [PrivateTasksScreen]
 // TODO When item is completed (isDone = true) move it down at the end of the list
